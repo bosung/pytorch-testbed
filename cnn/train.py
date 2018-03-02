6f6606fb37f090eb55fa87eaad03c8c21981543a
bosung <bosung.kim3416@gmail.com>
2018-03-02 22:59:50 +0900

    add train, test methods

--- cnn/train.py
+++ cnn/train.py
@@ -1,4 +1,6 @@
 import torch
+import torch.autograd as autograd
+import torch.nn as nn
 import torch.optim as optim
 import torchtext.vocab as vocab
 from random import shuffle
@@ -14,10 +16,27 @@ glove = vocab.GloVe(name='6B', dim=WORD_EMBED_DIM)
 
 
 def get_glove_vector(word):
+    """Return glove vector corresponding input word
+
+    Arguments:
+        word: input string
+
+    Example:
+        >>> get_glove_vector('the')
+        [torch.FloatTensor of size 100]
+    """
     try:
         return glove.vectors[glove.stoi[word]]
-    except ValueError:
+    except KeyError:
         return torch.FloatTensor(torch.randn(WORD_EMBED_DIM))
+
+
+def get_sentence_matrix(words):
+    """make (n, k) representaion of sentence
+    """
+    _sent_matrix = [get_glove_vector(word).view(1, 1, 1, -1) for word in words]
+    _sent_matrix = torch.cat(_sent_matrix, dim=2)
+    return _sent_matrix
 
 
 def train(train_data_idx):
@@ -25,15 +44,53 @@ def train(train_data_idx):
     optimizer = optim.SGD(model.parameters(), lr=0.001)
     loss_function = nn.NLLLoss()
 
-    for epoch in range(10):
-        model.zero_grad()
-
+    for epoch in range(30):
+        total_loss = 0
         for i in train_data_idx:
-            # input (mini_batch, in_channel, ix)
-            _input = [get_glove_vector[word] for word in train_data[i][0]]
-            _input = torch.cat(_input, dim=0)
-            model(input)
+            model.zero_grad()
+            optimizer.zero_grad()
 
+            # input (mini_batch, in_channel, ix)
+            #_input = [get_glove_vector(word).view(1, 1, 1, -1) for word in train_data[i][0]]
+            #_input = torch.cat(_input, dim=2)
+            s_m = get_sentence_matrix(train_data[i][0])
+
+            tag_score = model(s_m)
+            target = autograd.Variable(
+                    torch.LongTensor([0]) if train_data[i][1][0] == 1
+                    else torch.LongTensor([1]))
+
+            #print(tag_score, target)
+
+            loss = loss_function(tag_score, target)
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.data
+            #if i%10 == 0:
+            #    print("\t%d loss: %.3f" % (i, loss.data))
+        print("\t epoch %d\ttotal loss: %.3f" % (epoch, total_loss))
+
+    return model
+
+
+def test(model, test_data_idx):
+    total_size = len(test_data_idx)
+    answer_cnt = 0
+    for i in test_data_idx:
+        sentence, target = train_data[i][0], train_data[i][1]
+        s_m = get_sentence_matrix(sentence)
+        result = model(s_m).view(-1, 1)
+        answer = autograd.Variable(torch.LongTensor(target))
+        #print(result, answer)
+
+        v, ret_idx = torch.max(result, 0)
+        v, ans_idx = torch.max(answer, 0)
+        #print(ret_idx, ans_idx)
+        if ret_idx.equal(ans_idx):
+            answer_cnt += 1
+
+    print("\t accuracy: %.3f" % (answer_cnt/total_size*100))
 
 # train_data[0] = list(sentence)
 # train_data[1] = list([1, 0])
@@ -42,14 +99,11 @@ train_data = [[sent, lable] for sent, lable in zip(train_data[0], train_data[1])
 #print(train_data[:3])
 shuffle(train_data)
 
-kf = KFold(n_split=10)
+kf = KFold(n_splits=10)
 kf.get_n_splits(train_data)
 
 for train_index, test_index in kf.split(train_data):
     print("TRAIN:", train_index, "TEST:", test_index)
-
-
-
-
-#model = cnn.CNNTextClassifier(1, 2, 300, 0.2)
+    model = train(train_index)
+    test(model, test_index)
 
