bb8861dc6eb6931e595a1499c8e4cdafb9a50091
bosung <bosung.kim3416@gmail.com>
2019-08-30 19:24:10 +0900

    change models __init__.py

--- onmt/models/__init__.py
+++ onmt/models/__init__.py
@@ -1,6 +1,7 @@
 """Module defining models."""
 from onmt.models.model_saver import build_model_saver, ModelSaver
 from onmt.models.model import NMTModel
+from onmt.models.model import CLSModel
 
 __all__ = ["build_model_saver", "ModelSaver",
-           "NMTModel", "check_sru_requirement"]
+           "NMTModel", "CLSModel", "check_sru_requirement"]
